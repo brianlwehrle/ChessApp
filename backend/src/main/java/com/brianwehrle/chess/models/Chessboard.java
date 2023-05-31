52780e3402e6ac1e95db4ab56f0f46da07808d9c
brianlwehrle <brianwehrle@arizona.edu>
2023-05-31 16:43:10 -0700

    adding illegal moves, pre refactoring so only chessboard interacts with Square

--- backend/src/main/java/com/brianwehrle/chess/models/Chessboard.java
+++ backend/src/main/java/com/brianwehrle/chess/models/Chessboard.java
@@ -12,6 +12,7 @@ public class Chessboard {
                             // simpler than a 2d array
     private ArrayList<Piece> blackPieces;
     private ArrayList<Piece> whitePieces;
+    private Piece capturedPiece;
 
     public ArrayList<Piece> getBlackPieces() {
         return blackPieces;
@@ -27,6 +28,7 @@ public class Chessboard {
         board = new Square[NUM_COLS * NUM_ROWS];
         whitePieces = new ArrayList<>();
         blackPieces = new ArrayList<>();
+        capturedPiece = null;
 
         for (int row = 0; row < NUM_ROWS; row++) {
             for (int col = 0; col < NUM_COLS; col++) {
@@ -37,9 +39,9 @@ public class Chessboard {
         initialSetup();
     }
 
-    public void movePiece(Move move) {
+    public void makeMove(Move move) {
         Piece movingPiece = move.start().getPiece();
-        Piece capturedPiece = move.end().getPiece();
+        capturedPiece = move.end().getPiece();
 
         move.start().setPiece(null);
         movingPiece.setCurSquare(move.end());
@@ -56,75 +58,19 @@ public class Chessboard {
         if (!movingPiece.hasMoved()) movingPiece.setHasMoved(true);
     }
 
-    private void initialSetup() {
-        // pawns
-        for (int i = 0; i < 8; i++) {
-            Piece blackPawn = new Pawn(Color.BLACK);
-            Piece whitePawn = new Pawn(Color.WHITE);
-            addPiece(whitePawn, 1, i);
-            addPiece(blackPawn, 6, i);
+    public void undoMove(Move move) {
+        Piece movingPiece = move.start().getPiece();
+
+        move.start().setPiece(movingPiece);
+        if (capturedPiece != null) {
+            move.end().setPiece(capturedPiece);
         }
 
-        // knights
-        Piece blackKnight1 = new Knight(Color.BLACK);
-        Piece blackKnight2 = new Knight(Color.BLACK);
-        Piece whiteKnight1 = new Knight(Color.WHITE);
-        Piece whiteKnight2 = new Knight(Color.WHITE);
-        addPiece(blackKnight1, 7, 1);
-        addPiece(blackKnight2, 7, 6);
-        addPiece(whiteKnight1, 0, 1);
-        addPiece(whiteKnight2, 0, 6);
 
-        // Rooks
-        Piece blackRook1 = new Rook(Color.BLACK);
-        Piece blackRook2 = new Rook(Color.BLACK);
-        Piece whiteRook1 = new Rook(Color.WHITE);
-        Piece whiteRook2 = new Rook(Color.WHITE);
-        addPiece(blackRook1, 7, 0);
-        addPiece(blackRook2, 7, 7);
-        addPiece(whiteRook1, 0, 0);
-        addPiece(whiteRook2, 0, 7);
 
-        // Bishops
-        Piece blackBishop1 = new Bishop(Color.BLACK);
-        Piece blackBishop2 = new Bishop(Color.BLACK);
-        Piece whiteBishop1 = new Bishop(Color.WHITE);
-        Piece whiteBishop2 = new Bishop(Color.WHITE);
-        addPiece(blackBishop1, 7, 2);
-        addPiece(blackBishop2, 7, 5);
-        addPiece(whiteBishop1, 0, 2);
-        addPiece(whiteBishop2, 0, 5);
 
-        // Kings
-        Piece whiteKing = new King(Color.WHITE);
-        Piece blackKing = new King(Color.BLACK);
-        addPiece(blackKing, 7, 3);
-        addPiece(whiteKing, 0, 3);
-
-        // Queens
-        Piece whiteQueen = new Queen(Color.WHITE);
-        Piece blackQueen = new Queen(Color.BLACK);
-        addPiece(blackQueen, 7, 4);
-        addPiece(whiteQueen, 0, 4);
-
-        for (int i = 0; i < 16; i++) {
-            if (board[i].getPiece() == null) continue;
-            whitePieces.add(board[i].getPiece());
-            //System.out.println("Added a " + board[i].getPiece().getColor() + " " + board[i].getPiece().getType() + " to square " + board[i].getRow() + ", " + board[i].getCol());
-        }
-
-        for (int i = 48; i < board.length; i++) {
-            if (board[i].getPiece() == null) continue;
-            blackPieces.add(board[i].getPiece());
-            //System.out.println("Added a " + board[i].getPiece().getColor() + " " + board[i].getPiece().getType() + " to square " + board[i].getRow() + ", " + board[i].getCol());
-        }
     }
 
-    private void addPiece(Piece piece, int row, int col) {
-        Square square = getSquareAt(row, col);
-        piece.setCurSquare(square);
-        square.setPiece(piece);
-    }
 
     public Square getSquareAt(int row, int col) {
         if (row > 7 || col > 7 || row < 0 || col < 0) return null;
@@ -156,5 +102,99 @@ public class Chessboard {
         }
 
         return res;
+    }
+
+    public String toString(ArrayList<Square> threatMap) {
+
+        String line = "";
+        String res = "";
+        String output = "";
+
+        for (int i = board.length - 1; i >= 0; i--) {
+            if (threatMap.contains(board[i])) {
+                output = "X";
+            } else {
+                output = "_";
+            }
+            line = "|" + output + line;
+
+            if (i % NUM_ROWS == 0) {
+                line += "|\n";
+                res += line;
+                line = "";
+            }
+        }
+
+        return res;
+    }
+
+    private void initialSetup() {
+        // pawns
+        for (int i = 0; i < 8; i++) {
+            Piece blackPawn = new Pawn(Color.BLACK);
+            Piece whitePawn = new Pawn(Color.WHITE);
+            setPieceAt(whitePawn, 1, i);
+            setPieceAt(blackPawn, 6, i);
+        }
+
+        // knights
+        Piece blackKnight1 = new Knight(Color.BLACK);
+        Piece blackKnight2 = new Knight(Color.BLACK);
+        Piece whiteKnight1 = new Knight(Color.WHITE);
+        Piece whiteKnight2 = new Knight(Color.WHITE);
+        setPieceAt(blackKnight1, 7, 1);
+        setPieceAt(blackKnight2, 7, 6);
+        setPieceAt(whiteKnight1, 0, 1);
+        setPieceAt(whiteKnight2, 0, 6);
+
+        // Rooks
+        Piece blackRook1 = new Rook(Color.BLACK);
+        Piece blackRook2 = new Rook(Color.BLACK);
+        Piece whiteRook1 = new Rook(Color.WHITE);
+        Piece whiteRook2 = new Rook(Color.WHITE);
+        setPieceAt(blackRook1, 7, 0);
+        setPieceAt(blackRook2, 7, 7);
+        setPieceAt(whiteRook1, 0, 0);
+        setPieceAt(whiteRook2, 0, 7);
+
+        // Bishops
+        Piece blackBishop1 = new Bishop(Color.BLACK);
+        Piece blackBishop2 = new Bishop(Color.BLACK);
+        Piece whiteBishop1 = new Bishop(Color.WHITE);
+        Piece whiteBishop2 = new Bishop(Color.WHITE);
+        setPieceAt(blackBishop1, 7, 2);
+        setPieceAt(blackBishop2, 7, 5);
+        setPieceAt(whiteBishop1, 0, 2);
+        setPieceAt(whiteBishop2, 0, 5);
+
+        // Kings
+        Piece whiteKing = new King(Color.WHITE);
+        Piece blackKing = new King(Color.BLACK);
+        setPieceAt(blackKing, 7, 3);
+        setPieceAt(whiteKing, 0, 3);
+
+        // Queens
+        Piece whiteQueen = new Queen(Color.WHITE);
+        Piece blackQueen = new Queen(Color.BLACK);
+        setPieceAt(blackQueen, 7, 4);
+        setPieceAt(whiteQueen, 0, 4);
+
+        for (int i = 0; i < 16; i++) {
+            if (board[i].getPiece() == null) continue;
+            whitePieces.add(board[i].getPiece());
+            //System.out.println("Added a " + board[i].getPiece().getColor() + " " + board[i].getPiece().getType() + " to square " + board[i].getRow() + ", " + board[i].getCol());
+        }
+
+        for (int i = 48; i < board.length; i++) {
+            if (board[i].getPiece() == null) continue;
+            blackPieces.add(board[i].getPiece());
+            //System.out.println("Added a " + board[i].getPiece().getColor() + " " + board[i].getPiece().getType() + " to square " + board[i].getRow() + ", " + board[i].getCol());
+        }
+    }
+
+    private void setPieceAt(Piece piece, int row, int col) {
+        Square square = getSquareAt(row, col);
+        piece.setCurSquare(square);
+        square.setPiece(piece);
     }
 }
--- backend/src/main/java/com/brianwehrle/chess/models/Game.java
+++ backend/src/main/java/com/brianwehrle/chess/models/Game.java
@@ -1,16 +1,18 @@
 package com.brianwehrle.chess.models;
 
-import com.brianwehrle.chess.models.pieces.Pawn;
 import com.brianwehrle.chess.models.pieces.Piece;
+import com.brianwehrle.chess.models.pieces.PieceType;
 
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Game {
 
-    private Chessboard board;
+    private final Chessboard board;
     private Player whitePlayer, blackPlayer, activePlayer;
     private ArrayList<Move> prevMoves;
+    private ArrayList<Square> threatMap; // list of squares currently under attack
+
 
     public Game(Player white, Player black) {
         this.whitePlayer = white;
@@ -22,42 +24,68 @@ public class Game {
 
         board = new Chessboard();
         prevMoves = new ArrayList<>();
+        threatMap = new ArrayList<>();
     }
 
     public void run() {
 
-        while (!isCheckmate()) {
+        while (true) {
             System.out.println(board);
+            System.out.println(board.toString(threatMap));
 
-            ArrayList<Move> legalMoves = getPossibleMoves(board, activePlayer);
+            ArrayList<Move> possibleMoves = getPossibleMoves(activePlayer, false);
+            removeIllegalMoves(possibleMoves);
+
+            // Game over!
+            if (possibleMoves.isEmpty()) {
+                System.out.println(activePlayer.getColor() + " loses!");
+                break;
+            }
 
             System.out.println(activePlayer.getColor() + " to move: ");
-            for (int i = 0; i < legalMoves.size(); i++) {
-                System.out.println(i+1 + ": " + legalMoves.get(i));
+            for (int i = 0; i < possibleMoves.size(); i++) {
+                System.out.println(i+1 + ": " + possibleMoves.get(i));
             }
 
             Scanner scanner = new Scanner(System.in);
-            String choice = scanner.next();
+            String nextMove = scanner.next();
 
-            board.movePiece(legalMoves.get(Integer.valueOf(choice) - 1));
-            prevMoves.add(legalMoves.get(Integer.valueOf(choice) - 1));
+            board.makeMove(possibleMoves.get(Integer.valueOf(nextMove) - 1));
+            prevMoves.add(possibleMoves.get(Integer.valueOf(nextMove) - 1));
+
+            threatMap.clear(); // get rid of old threat map
+            getPossibleMoves(activePlayer, true); // has only the effect of creating a new threat map.
 
             activePlayer = (activePlayer == whitePlayer ? blackPlayer : whitePlayer);
         }
     }
 
-    private ArrayList<Move> getPossibleMoves(Chessboard board, Player activePlayer) {
+    private void removeIllegalMoves(ArrayList<Move> possibleMoves) {
+        // king cannot go into check
+        possibleMoves.removeIf(move -> move.start().getPiece().getType() == PieceType.KING && isUnderAttack(move.end()));
+
+        // simulate each move and see if my king would be in check
+        for (Move move : possibleMoves) {
+            Chessboard tempBoard = board;
+            ArrayList<Square> tempThreatMap = threatMap;
+
+            tempBoard.makeMove(move);
+
+        }
+    }
+
+    private ArrayList<Move> getPossibleMoves(Player activePlayer, boolean newThreatMap) {
         ArrayList<Move> moves = new ArrayList<>();
         ArrayList<Piece> curPieces =  (activePlayer.getColor() == Color.WHITE) ? board.getWhitePieces() : board.getBlackPieces();
 
         for (Piece piece : curPieces) {
-            moves.addAll(getPossiblePieceMoves(board, piece));
+            moves.addAll(getPossiblePieceMoves(piece, newThreatMap));
         }
 
         return moves;
     }
 
-    private ArrayList<Move> getPossiblePieceMoves(Chessboard board, Piece piece) {
+    private ArrayList<Move> getPossiblePieceMoves(Piece piece, boolean newThreatMap) {
         ArrayList<Move> moves = new ArrayList<>();
 
         switch (piece.getType()) {
@@ -75,6 +103,8 @@ public class Game {
                         } else {
                             moves.add(new Move(start, nextSquare));
                         }
+
+                        if (newThreatMap) threatMap.add(nextSquare);
                     }
                 }
             }
@@ -99,8 +129,11 @@ public class Game {
                 //attacking
                 for (int dx = -1; dx <= 1; dx += 2) { // just checks both forward diagonals
                     nextSquare = board.getSquareAt(start.getRow() + dy, start.getCol() + dx);
-                    if (nextSquare != null && nextSquare.getPiece() != null) {
-                        moves.add(new Move(start, nextSquare));
+                    if (nextSquare != null) {
+                        if (newThreatMap) threatMap.add(nextSquare);
+                        if (nextSquare.getPiece() != null) {
+                            moves.add(new Move(start, nextSquare));
+                        }
                     }
                 }
 
@@ -108,10 +141,13 @@ public class Game {
             case BISHOP, ROOK, QUEEN -> {
                 Square start = piece.getCurSquare();
 
+                // for each direction build a path of moves until you hit another piece
                 for (Pair direction : piece.getDirections()) {
                     Square nextSquare = board.getSquareAt(start.getRow() + direction.dy(), start.getCol() + direction.dx());
 
                     for (int scalar = 2; nextSquare != null; scalar++) {
+                        if (newThreatMap) threatMap.add(nextSquare);
+
                         if (nextSquare.getPiece() == null) {
                             moves.add(new Move(start, nextSquare));
                         } else if (nextSquare.getPiece().getColor() != piece.getColor()) {
@@ -130,7 +166,7 @@ public class Game {
         return moves;
     }
 
-    private boolean isCheckmate() {
-        return false;
+    private boolean isUnderAttack(Square square) {
+        return threatMap.contains(square);
     }
 }
